4e3b7c75e5b9f4bfd08a63ff51909c2c9b5f8710
Ólafur Páll Geirsson <olafurpg@gmail.com>
2015-05-26 23:30:25 +0200

    Add test for fullJoin, left and right joins are missing

--- slick-direct/src/test/scala/ch/epfl/lamp/slick/direct/JoinSpec.scala
+++ slick-direct/src/test/scala/ch/epfl/lamp/slick/direct/JoinSpec.scala
@@ -27,46 +27,48 @@ class JoinSpec extends FlatSpec with TestHelper {
     )
   }
 
-  // TODO: Implement LiftedTable.*: ProvenShape
-  //    [info] - should work *** FAILED ***
-  //    [info]   scala.NotImplementedError: an implementation is missing
-  //  [info]   at scala.Predef$.$qmark$qmark$qmark(Predef.scala:225)
-  //  [info]   at ch.epfl.lamp.slick.direct.Query$LiftedTable$1.$times(Direct.scala:103)
-  //  "leftOuterJoin" should "work" in {
-  //    val users = Query[User]
-  //    val cars = Query[Car]
-  //    val d = queryDebug {
-  //      users joinLeft cars on (_.id == _.ownerId)
-  //    }
-  //    equalQueries(
-  //      d.result,
-  //      (liftedUsers joinLeft liftedCars).result
-  //    )
-  //  }
+  "fullOuterJoin" should "work" in {
+    val users = Query[User]
+    val cars = Query[Car]
+    val d = queryDebug {
+      users joinFull cars on (_.id == _.ownerId)
+    }
+    equalQueries(
+      d.result,
+      (liftedUsers joinFull liftedCars on (_.id === _.ownerId)).result
+    )
+  }
 
-  //  "rightOuterJoin" should "work" in {
-  //    val users = Query[User]
-  //    val cars = Query[Car]
-  //    val d = queryDebug {
-  //      users joinRight cars on (_.id == _.ownerId)
-  //    }
-  //    equalQueries(
-  //      d.result,
-  //      (liftedUsers joinRight liftedCars).result
-  //    )
-  //  }
-  //
-  //  "fullOuterJoin" should "work" in {
-  //    val users = Query[User]
-  //    val cars = Query[Car]
-  //    val d = queryDebug {
-  //      users joinFull cars on (_.id == _.ownerId)
-  //    }
-  //    equalQueries(
-  //      d.result,
-  //      (liftedUsers joinFull liftedCars).result
-  //    )
-  //  }
+  // TODO: We need to solve this dsl.compile mess
+  // This will need to expand to arbitrarily nested types
+//    [error] /Users/ollie/Dropbox/epfl/2014-2015/spring/lamp/slick-direct/slick-direct/src/test/scala/ch/epfl/lamp/slick/direct/JoinSpec.scala:38: type mismatch;
+//  [error]  found   : ch.epfl.lamp.slick.direct.BaseJoinQuery[slick.lifted.AbstractTable[ch.epfl.lamp.slick.direct.User]#TableElementType,Option[ch.epfl.lamp.slick.direct.Car],slick.lifted.AbstractTable[ch.epfl.lamp.slick.direct.User]#TableElementType,Option[ch.epfl.lamp.slick.direct.Car],Seq]
+//  [error]  required: ch.epfl.lamp.slick.direct.Query[(ch.epfl.lamp.slick.direct.User, Option[ch.epfl.lamp.slick.direct.Car]),Seq]
+//  [error]       val d = queryDebug {
+//    [error]                          ^
+//    "leftOuterJoin" should "work" in {
+//      val users = Query[User]
+//      val cars = Query[Car]
+//      val d = queryDebug {
+//        users joinLeft cars on (_.id == _.ownerId)
+//      }
+//      equalQueries(
+//        d.result,
+//        (liftedUsers joinLeft liftedCars).result
+//      )
+//    }
+
+//    "rightOuterJoin" should "work" in {
+//      val users = Query[User]
+//      val cars = Query[Car]
+//      val d = queryDebug {
+//        users joinRight cars on (_.id == _.ownerId)
+//      }
+//      equalQueries(
+//        d.result,
+//        (liftedUsers joinRight liftedCars).result
+//      )
+//    }
 
   // TODO: [error] /Users/ollie/Dropbox/epfl/2014-2015/spring/lamp/slick-direct/slick-direct/src/test/scala/ch/epfl/lamp/slick/direct/JoinSpec.scala:52: Unable to resolve type for (_1: ch.epfl.lamp.slick.direct.User, _2: Option[ch.epfl.lamp.slick.direct.Car])(ch.epfl.lamp.slick.direct.User, Option[ch.epfl.lamp.slick.direct.Car])((u @ _), (c @ _))
   //    [error]           (u, c) <- users joinLeft cars on (_.id == _.ownerId)
